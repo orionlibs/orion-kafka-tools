65335cac692c24c351f0d18676205a40c108c53e
Dimitrios Efthymiou <efthymiou.dimitrios1@gmail.com>
2025-01-15 12:17:56 +0000

    initial commit

---
+++ src/main/java/io/github/orionlibs/orion_kafka_tools/Utils.java
@@ -0,0 +1,26 @@
+package io.github.orionlibs.orion_kafka_tools;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class Utils
+{
+    public static void nonblockingDelay(int numberOfSeconds)
+    {
+        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+        CompletableFuture<Void> delayedTask = CompletableFuture.runAsync(() -> {
+        }).thenCompose(ignored -> delay(numberOfSeconds, TimeUnit.SECONDS, scheduler));
+        delayedTask.join(); //wait for the task to complete
+        scheduler.shutdown();
+    }
+
+
+    private static CompletableFuture<Void> delay(long delay, TimeUnit unit, ScheduledExecutorService scheduler)
+    {
+        CompletableFuture<Void> future = new CompletableFuture<>();
+        scheduler.schedule(() -> future.complete(null), delay, unit);
+        return future;
+    }
+}
